b5f27bc389afdf60938b55c7b60d6c2fe1e5030d
Trevor Saunders <tbsaunde+binutils@tbsaunde.org>
2016-05-15 03:29:22 -0400

    tilepro: make field type enum

--- include/opcode/tilepro.h
+++ include/opcode/tilepro.h
@@ -1469,7 +1469,7 @@ struct tilepro_operand
   tilepro_operand_type type;
 
   /* The default relocation type for this operand.  */
-  signed int default_reloc : 16;
+  bfd_reloc_code_real_type default_reloc : 16;
 
   /* How many bits is this value? (used for range checking) */
   unsigned int num_bits : 5;
